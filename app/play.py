286fbcdf41ce292fd4accb8844f15a75d7ba607f
muzamilsulthana2097-ops <muzamilsulthana2097@gmail.com>
2026-09-08 12:11:07 +0530

    Update play.py

--- app/play.py
+++ app/play.py
@@ -7,4 +7,20 @@ def get_vid(query)
 
     try:
          encode = urllib.parse.quote(query)
-      
+
+         ur1 = (
+               "https://www.youtube.com/results"
+               "?search_query=" +encode
+         )
+
+          request = urllib.request.Request(
+              url,
+              headers=(
+                      "User- Agent": Mozilla/5.0"
+              }
+         )
+
+          data = urllib.request.urlopen(
+              request,
+              timeout=5
+              
