f1da1fd3df60e5d913f2b69a06ed0c9a1617a6ce
muzamilsulthana2097-ops <muzamilsulthana2097@gmail.com>
2026-09-08 15:07:53 +0530

    Create play.py

---
+++ app/youtube/play.py
@@ -0,0 +1,74 @@
+import re
+import urllib.parse
+import urllib.request
+
+
+def get_vid(query)
+
+    try:
+         encode = urllib.parse.quote(query)
+
+         ur1 = (
+               "https://www.youtube.com/results"
+               "?search_query=" +encode
+         )
+
+          request = urllib.request.Request(
+              url,
+              headers=(
+                      "User- Agent": Mozilla/5.0"
+              }
+         )
+
+          data = urllib.request.urlopen(
+              request,
+              timeout=5
+          ).read().decode("utf-8",errors="ignore')
+
+          ids = re.findall(
+              r'"videoId';"([^"]+)"',
+              data
+          )
+
+          retrun ids[0] if ids else None
+
+      expcept Exception:
+         retrun None
+
+def create_youtube_ur1(command):
+    
+       text = command.lower().stip()
+
+
+       patterns = [
+            r"play\s+song\s+(.+)",
+            r"play\s+music\s+(.+)",
+            r"play\s+(.+)",                                                                                                                                                                                                 
+            r"youtube\s+(.+)"
+       ]
+
+       query = command
+
+       for pattern in patterns:
+
+           match = research(
+                pattern,
+                text
+           )
+
+           if match :
+              query = match.group(1)
+               break
+
+      query = query.strip()
+
+    video_id = get_vid(query)
+
+    if not video_id:
+        return None
+
+    return (
+         "https://www.youtube.com/embed/"
+          +video_id
+          +"?autoplay = 1&mute=0"
+    )
