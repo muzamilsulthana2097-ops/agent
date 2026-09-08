77980bd99bf1ce82019d07cbcd85770ece036b9b
muzamilsulthana2097-ops <muzamilsulthana2097@gmail.com>
2026-09-08 12:14:54 +0530

    Update play.py

--- app/play.py
+++ app/play.py
@@ -23,4 +23,9 @@ def get_vid(query)
           data = urllib.request.urlopen(
               request,
               timeout=5
-              
+          ).read().decode("utf-8",errors="ignore')
+
+          ids = re.findall(
+              r'"videoId';"([^"]+)"',
+              data
+          )
