ce29e58e3c05ad77085e7583c89743c82136f127
muzamilsulthana2097-ops <muzamilsulthana2097@gmail.com>
2026-09-10 15:46:36 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,90 @@
-from flask import Flask, render_template
-from app.youtube import youtube_bp
+import os
 
+from flask import Flask,request,jsonify,render_template
+from flask_cors import CORS
+
+from app.gmail import (
+    is_email_command,
+    extract_email,
+    create_gmail_url,
+    generatw_email_with_gemimi
+)
+
+from app.youtube import youtube_bp
 
 def create_app():
 
     app = Flask(__name__)
+    CORS(app)
 
+    #youTube
     app.register_blueprint(
         youtube_bp,
         url_prefix="/youtube"
     )
 
+    #home
     @app.route("/")
     def home():
         return render_template("index.html")
 
+    #HTML
     @app.route("/html")
     def html():
         return render_template("index.html")
 
-    return app
+    #HEALTH
+    @app.route("/health")
+    def health();
+        return jsonify({
+            "status":"ok",
+            "SERVICE":"Nova AI Agent"
+        })
+
+   #GMAIL AI AGENT
+   @app.route("/agent,methods=["POST"])
+   def agent():
+
+       try:
+            data = request.get_json(slent=true) or {}
+            command = data.get("command","").strip()
+
+            if not command
+               return jonisfy({
+                    "succes":False,
+                    "message":"command is required"
+                }),400
+
+            if not is_email_command(commmand):
+              return jsonify({
+                    "success":Flase,
+                    "message":"please give a Gmail comand".
+                }),400
+
+        recipient = extract_email(command)
+
+        email = generate_email_with_gemini(command)
+
+        return jsonify({
+            "success": True,
+            "type": "email",
+            "eamil_generated":True,
+            "recipient": recipient,
+            "subject":email["subject"]
+            "body":email["body"]
+            "gmail_url":create_gamil_url(
+                email["subject"],
+                email["body"]
+                recipient
+            )
+        })
+
+    except Exception as e:
+
+    return jsonify({
+        "success":False,
+        "message":str(e)
+    }),500
+
+return app
+          
