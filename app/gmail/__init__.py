c1bfe6a822047f53a206c78959ef8ec78e717d5a
muzamilsulthana2097-ops <muzamilsulthana2097@gmail.com>
2026-09-11 09:31:16 +0530

    Update __init__.py

--- app/gmail/__init__.py
+++ app/gmail/__init__.py
@@ -1,2 +1,2 @@
-from .gamil_write import is_email_command, extract-email,create_gamil_url1
+from .gamil_write import is_email_command, extract_email,create_gamil_url
 from .gamil_gen import generate_email_with_gemini
