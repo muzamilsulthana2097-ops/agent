25d19c0de7cf0f1ffb38cd6f19e79e3b430ba388
muzamilsulthana2097-ops <muzamilsulthana2097@gmail.com>
2026-09-11 09:42:48 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -2,37 +2,38 @@ import os
 import re
 import urllib.parse
 
-KEYWORDS= (
-  "gamil","email","e-mail","mail",
-  "writean email","send an email","draft an email",
-  "compose an email","write mail","send mail","draft mail",
-  "compose email"
+
+KEYWORDS = (
+    "gmail", "email", "e-mail", "mail",
+    "write an email", "send an email", "draft an email",
+    "compose an email", "write mail", "send mail", "draft mail",
+    "compose mail"
 )
 
 def is_email_command(text):
-     text = text.lower()
-  retrun any (k in text for k in KEYWORDS)
+    text = text.lower()
+    return any(k in text for k in KEYWORDS)
 
 def extract_email(text):
-match = re.search(r"[\w.+-]+@[q\w.-]+\.\w+",text)
-if match:
-  return match.group(0)
+    match = re.search(r"[\w.+-]+@[\w.-]+\.\w+", text)
+    if match:
+        return match.group(0)
 
-match = re.search(
-  r"([\w.+_]+}\start|+([\w.+])\s+dot\s+(\w+)",
-  text.lower()
-)
-if match:
-    return f"{match.group(1)}@{match.group(2)}.{match.group(0)}"
+    match = re.search(
+        r"([\w.+-]+)\s+at\s+([\w.-]+)\s+dot\s+(\w+)",
+        text.lower()
+    )
+    if match:
+        return f"{match.group(1)}@{match.group(2)}.{match.group(3)}"
 
-retrun ""
+    return ""
 
-def create_gamil_url(subject="",body="",recipient=""):
-  params = urllib.parse.urlencode({
-      "view":"cm",
-    "fs";"1",
-  "to":recipient,
-  "su":subject,
-  "body":body
-})
-return f"https://mail.google.com/mail/U/0?{params}"
+def create_gmail_url(subject="", body="", recipient=""):
+    params = urllib.parse.urlencode({
+        "view": "cm",
+        "fs": "1",
+        "to": recipient,
+        "su": subject,
+        "body": body
+    })
+    return f"https://mail.google.com/mail/u/0/?{params}"
